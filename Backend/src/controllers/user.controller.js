4f48e670e84dd709c4d24745efff909fa133041b
Rohithprakashdiv <Rohithprakash19@gmail.com>
2025-11-23 20:54:47 +0530

    implement secure login flow with JWT and HttpOnly cookies

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -1,41 +1,97 @@
-import User from "../models/User.js"
-import bcrypt from "bcryptjs"
-import jwt from "jsonwebtoken"
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
-export const registerUser  = async(req,res)=>{
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
 
-    try {
-        const { name, email, password } = req.body;
-
-        if(!name || !email || !password){
-           return res.status(400).json({
-             success:false,
-             message:"Name, email, and password are required"
-           })
-        }
-
-        const userExists = await User.findOne({email})
-        if(userExists){
-            return res.status(400).json({
-                success:false,
-                message:"Email already registered",
-            })
-        }
-
-        const hashedPassword  = await bcrypt.hash(password,10);
-
-        const newUser = await User.create({
-            name,
-            email, 
-            password:hashedPassword,
-            avatar:"https://res.cloudinary.com/dgrxeqayx/image/upload/v1763826800/user_nkqqfm.png"
-        })
-
-        await newUser.save()
-       res.status(200).json({success:true,message:'User created successfully',user:newUser})
-    } catch (error) {
-        console.error(error)
-        res.status(500).json({ success: false, message: 'Server error' });
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Name, email, and password are required",
+      });
     }
 
-}
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({
+        success: false,
+        message: "Email already registered",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      avatar:
+        "https://res.cloudinary.com/dgrxeqayx/image/upload/v1763826800/user_nkqqfm.png",
+    });
+
+    await newUser.save();
+    res
+      .status(200)
+      .json({
+        success: true,
+        message: "User created successfully",
+        user: newUser,
+      });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+};
+
+export const userLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "email, and password are required",
+      });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    res.status(200).json({
+      message: "Login successful",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        avatar: user.avatar,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    console.log("Login Error:", error.message);
+    res.status(500).json({ message: "Server error" });
+  }
+};
--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
 const router = express.Router();
-import { registerUser } from "../controllers/user.controller.js";
+import { registerUser, userLogin } from "../controllers/user.controller.js";
 
 router.post('/auth/signup',registerUser)
+router.post('/auth/login',userLogin)
 
 
 export default router;
