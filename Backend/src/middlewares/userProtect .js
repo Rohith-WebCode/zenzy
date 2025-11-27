18daaabb1fee6f941d4e765205cce77f6f71f92c
Rohithprakashdiv <Rohithprakash19@gmail.com>
2025-11-27 15:00:27 +0530

    add user and admin authentication middleware

---
+++ Backend/src/middlewares/userProtect .js
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User";
+
+export const protect = async (req, res, next) => {
+  try {
+    const token = req.cookies.token;
+
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Not authorized, no token",
+      });
+    }
+
+    const decode = jwt.decode(token, process.env.JWT_SECRET),
+    user = await User.findById(decode.id).select("-password");
+
+    if (!user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid or expired token",
+    });
+  }
+};
